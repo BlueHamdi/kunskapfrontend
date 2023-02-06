6ba7a3b3076da50d65f4eea249d91bc257060f26
Ibrahimaden00 <Ibrahimaden00@gmail.com>
2023-02-06 20:12:11 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ root.render(
   <Routes>
     <Route path='/' element= {<App/>}/>
     <Route path="/play" element= {<Play/>}/>
-    <Route path="/Scorefetching" element= {<Scorefetching/>}/>
+    <Route path="/ScoreFetching" element= {<Datafetching/>}/>
 
     <Route path="/learn" element= {<Learn/>}/>
   </Routes>
