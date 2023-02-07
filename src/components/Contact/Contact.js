265c2e4aebd2d4e685648eea45e141044cfd9be3
Oras94 <93916020+Oras94@users.noreply.github.com>
2023-02-07 16:43:19 +0100

    CONTACT ÄNDRING

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,13 +3,13 @@ import './Contact.css';
 
 const Contact = () => {
   return (
-    <div className="contact-info-container">
-      <h2 className="contact-info-header">Contact Us</h2>
+    <div className="contact-container">
+      <h2 className="contact-header">Contact Us</h2>
       <div className="contact-details">
         <p><strong>Email:</strong> Kunskapsvarvet@gmail.com</p>
         <p><strong>Phone:</strong> (46) xxx-xxxx</p>
         <p><strong>Address:</strong> Yrkeshögskola Newton</p>
-        <p className="contact-help-text">For help, please email us at <a href="mailto:kunskapsvarvet@help.com" className="contact-info-support-link">kunskapsvarvet@help.com</a></p>
+        <p className="contact-help-text">For help, please email us at <a href="mailto:kunskapsvarvet@help.com" className="contact-help-link">kunskapsvarvet@help.com</a></p>
       </div>
     </div>
   );
